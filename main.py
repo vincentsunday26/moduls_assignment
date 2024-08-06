1e7996d37f6230f7fa58572918dcabd381e6386a
vybz26 <Vincentsunday7000@outlook.com>
2024-08-06 18:15:18 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import mood
+
+user_mood = input("how are you feeling today ?  ")
+
+print(mood.mood_responses(user_mood))
---
+++ mood.py
@@ -0,0 +1,12 @@
+# mood.py
+
+def mood_responses(mood):
+    if mood == "happy":
+        return "That's awesome!"
+
+    elif mood == "angry":
+        return "It's okay to feel angry sometimes. I am sorry you are angry"
+    else:
+        return "I don't know that mood, but I'm here for you!"
+
+
